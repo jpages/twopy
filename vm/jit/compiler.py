2117cdb4792ad3c6f1da96f96bb89f15026df6c8
Julien Pagès <julien.projet@gmail.com>
2017-11-16 10:50:52 -0500

    Saving

--- vm/jit/compiler.py
+++ vm/jit/compiler.py
@@ -12,20 +12,24 @@ import peachpy.x86_64 as asm
 def compile(function):
     print("Compilation of function " + str(function))
 
-    x = peachpy.Argument(peachpy.int32_t)
-    y = peachpy.Argument(peachpy.int32_t)
+    # TODO: for now all parameter are 32 bits integers
+    arguments = []
+    for i in range(function.argcount):
+        arguments.append(peachpy.Argument(peachpy.int32_t))
 
-    with asm.Function("Add", (x, y), peachpy.int32_t) as asm_function:
-        reg_x = asm.GeneralPurposeRegister32()
-        reg_y = asm.GeneralPurposeRegister32()
+    print("Arguments for the function " + str(function) + " : " + str(arguments))
 
-        asm.LOAD.ARGUMENT(reg_x, x)
-        asm.LOAD.ARGUMENT(reg_y, y)
+    # with asm.Function("Add", (x, y), peachpy.int32_t) as asm_function:
+    #     reg_x = asm.GeneralPurposeRegister32()
+    #     reg_y = asm.GeneralPurposeRegister32()
+    #
+    #     asm.LOAD.ARGUMENT(reg_x, x)
+    #     asm.LOAD.ARGUMENT(reg_y, y)
+    #
+    #     asm.ADD(reg_x, reg_y)
+    #
+    #     asm.RETURN(reg_x)
+    #
+    # python_function = asm_function.finalize(asm.abi.detect()).encode().load()
 
-        asm.ADD(reg_x, reg_y)
-
-        asm.RETURN(reg_x)
-
-    python_function = asm_function.finalize(asm.abi.detect()).encode().load()
-
-    print(python_function(2, 4)) # -> prints "6"
+    # print(python_function(2, 4)) # -> prints "6"
