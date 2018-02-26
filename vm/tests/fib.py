f3694c486666a481151ade853a87d5cabf9fc4b8
Julien Pagès <julien.projet@gmail.com>
2018-02-26 12:19:17 -0500

    Saving

--- vm/tests/fib.py
+++ vm/tests/fib.py
@@ -4,11 +4,17 @@ def fact(n):
     else:
         return n*fact(n-1)
 
-def fib(n):
-    a,b = 1,1
-    for i in range(n):
-        a,b = b,a+b
-    return a
+# def fib(n):
+#     a,b = 1,1
+#     for i in range(n):
+#         a,b = b,a+b
+#     return a
+
+def test():
+    if 2 < 3:
+        return 1
+    else:
+        return 2
 
 def fibR(n):
     if n<2:
@@ -16,10 +22,9 @@ def fibR(n):
     else:
         return fibR(n-1)+fibR(n-2)
 
-print(fact(5))
 
-fact(5)
+print(fibR(40))
 
-print(fib(100))
-print(fib(10))
-print(fibR(10))
+#print(fib(20))
+#print(fib(10))
+#print(fibR(10))
