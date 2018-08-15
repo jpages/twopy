7fecb175585ba8228f9770a4ce8d86042edc0971
Julien Pagès <julien.projet@gmail.com>
2018-08-15 13:42:23 -0400

    wip

--- vm/benchmarks/tak.py
+++ vm/benchmarks/tak.py
@@ -1,33 +1,50 @@
 def tak(x, y, z):
-    # print(x)
     if not y < x:
         return z
     else:
         return tak(tak(x-1, y, z), tak(y-1, z, x), tak(z-1, x, y))
 
 
-print(tak(5, 4, 3))
-print(tak(9, 6, 3))
 print(tak(18, 12, 6))
 print(tak(27, 18, 9))
+print(tak(36, 27, 18))
+print(tak(45, 36, 27))
+print(tak(54, 45, 36))
+print(tak(63, 54, 45))
+print(tak(72, 63, 54))
+print(tak(81, 72, 63))
+print(tak(90, 81, 72))
 
-print(tak(5, 4, 3))
-print(tak(9, 6, 3))
-print(tak(18, 12, 6))
-print(tak(27, 18, 9))
 
-print(tak(5, 4, 3))
-print(tak(9, 6, 3))
 print(tak(18, 12, 6))
 print(tak(27, 18, 9))
+print(tak(36, 27, 18))
+print(tak(45, 36, 27))
+print(tak(54, 45, 36))
+print(tak(63, 54, 45))
+print(tak(72, 63, 54))
+print(tak(81, 72, 63))
+print(tak(90, 81, 72))
 
-print(tak(5, 4, 3))
-print(tak(9, 6, 3))
-print(tak(18, 12, 6))
-print(tak(27, 18, 9))
 
-print(tak(5, 4, 3))
-print(tak(9, 6, 3))
 print(tak(18, 12, 6))
 print(tak(27, 18, 9))
+print(tak(36, 27, 18))
+print(tak(45, 36, 27))
+print(tak(54, 45, 36))
+print(tak(63, 54, 45))
+print(tak(72, 63, 54))
+print(tak(81, 72, 63))
+print(tak(90, 81, 72))
+
+
+print(tak(18, 12, 6))
+print(tak(27, 18, 9))
+print(tak(36, 27, 18))
+print(tak(45, 36, 27))
+print(tak(54, 45, 36))
+print(tak(63, 54, 45))
+print(tak(72, 63, 54))
+print(tak(81, 72, 63))
+print(tak(90, 81, 72))
 
