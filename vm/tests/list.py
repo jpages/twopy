516d8ca771cf9f8d66426af3ca1c584c52b638ed
Julien Pagès <julien.projet@gmail.com>
2018-12-11 09:44:23 -0500

    List test

--- vm/tests/list.py
+++ vm/tests/list.py
@@ -14,9 +14,10 @@ test.list_put(40, 0)
 test.list_put(41, 1)
 test.list_put(42, 2)
 
-test.list_get(0)
-test.list_get(1)
-test.list_get(2)
+
+print(test.list_get(0))
+print(test.list_get(1))
+print(test.list_get(2))
 
 # test[0] = 2
 
